686704d69d1a2c7d977ce267ed043520a7c3e549
whj1118 <whj1118@gmail.com>
2022-09-20 18:45:55 +0900

    지도 핀, 주소 검색, 리덕스,리덕스 툴킷, 리덕스 persist, 리덕스 wrapper

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,7 +20,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <Script
-            type="text/javascript"
+            strategy="beforeInteractive"
             src={`https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NEXT_PUBLIC_MAP_KEY}`}
           ></Script>
         </Head>
