4fc5264ebdf0cd421994fec562775907654d3289
whj1118 <whj1118@gmail.com>
2022-09-26 10:12:12 +0900

    [Feat] AS 요청 , 확인, 리뷰

--- components/mypage/as/AsRequestEndReview.tsx
+++ components/mypage/as/AsRequestEndReview.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import AsRequest from './AsRequest';
+import AsRequestWriteReview from './AsRequestWriteReview';
 
 type Props = {};
 
 const AsRequestEndReview = (props: Props) => {
-  return <div>AsRequestEndReview</div>;
+  return (
+    <>
+      <AsRequest />
+      <AsRequestWriteReview />
+    </>
+  );
 };
 
 export default AsRequestEndReview;
