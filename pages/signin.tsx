50ae8afef3789cdfe3c5d3c1b1ae9aab2afa1089
whj1118 <whj1118@gmail.com>
2022-09-29 19:00:27 +0900

    네이버

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -38,6 +38,27 @@ const Signin = (props: Props) => {
     login(naverLogin, function (naverLogin) {
       const hash = router.asPath.split('#')[1]; // 네이버 로그인을 통해 전달받은 hash 값
       console.log('hash -> ' + hash);
+
+      const token = hash.split('=')[1].split('&')[0]; // token값 확인
+      console.log('토큰입니다 => ' + token);
+      naverLogin.getLoginStatus((status: any) => {
+        if (status) {
+          // 로그인 상태 값이 있을 경우
+          console.log('[로그인상태값] 네이버 => ' + status);
+          console.log('[whj] 네이버 로그인 데이터 => ' + naverLogin);
+          // let email = naverLogin.user.getEmail();
+          // NaverApi(naverLogin, function (result) {
+          //   callBack(result);
+          // });
+          // // /naver 페이지로 token값과 함께 전달 (서비스할 땐 token 전달을 하지 않고 상태 관리를 사용하는 것이 바람직할 것으로 보임)
+          // Router.push({
+          //   pathname: '/signin',
+          //   query: {
+          //     token: token,
+          //   },
+          // });
+        }
+      });
     });
 
     // naverResponse === undefined
