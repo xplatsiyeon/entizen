217cbf1f5a3eb38d3da978be87fb7f81c3670129
whj1118 <whj1118@gmail.com>
2022-09-30 12:18:32 +0900

    script 변경

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -42,6 +42,12 @@ class MyDocument extends Document {
         <body>
           <Main />
           <script type="text/javascript" src="/testsss.js"></script>
+          <script
+            defer
+            type="text/javascript"
+            src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.0.js"
+            charSet="utf-8"
+          ></script>
           <NextScript />
         </body>
       </Html>
