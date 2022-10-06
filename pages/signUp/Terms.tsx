7e659d7c0c75ea277ef45b00272c0ca7cfb6904d
whj1118 <whj1118@gmail.com>
2022-10-06 10:16:39 +0900

    사파리 인증 오류 test

--- pages/signUp/Terms.tsx
+++ pages/signUp/Terms.tsx
@@ -255,8 +255,9 @@ const SignUpTerms = () => {
               </Box>
             </BottomForm>
             <div>
-              <form name="form_chk" method="post">
+              <form name="form_chk" method="get">
                 <input type="hidden" name="m" value="checkplusService" />
+
                 {/* <!-- 필수 데이타로, 누락하시면 안됩니다. --> */}
                 <input
                   type="hidden"
@@ -264,6 +265,7 @@ const SignUpTerms = () => {
                   name="EncodeData"
                   value={data !== undefined && data}
                 />
+                <input type="hidden" name="recvMethodType" value="get" />
                 {/* <!-- 위에서 업체정보를 암호화 한 데이타입니다. --> */}
                 <Btn
                   text="본인인증하기"
