eaaf1eb15c55727ae5cbd597c312102883f19236
whj1118 <whj1118@gmail.com>
2022-10-07 12:27:47 +0900

    axios 변경

--- pages/find/password2.tsx
+++ pages/find/password2.tsx
@@ -70,7 +70,8 @@ const FindPassword = () => {
 
   const onClickButton = async () => {
     console.log('비밀번호 함수 실행');
-    const PROFILE_API = 'https://test-api.entizen.kr/api/members';
+    const PROFILE_API =
+      'https://test-api.entizen.kr/api/members/reset/password';
     const accessToken = JSON.parse(localStorage.getItem('ACCESS_TOKEN')!);
     let key = localStorage.getItem('key');
     let data = JSON.parse(key!);
