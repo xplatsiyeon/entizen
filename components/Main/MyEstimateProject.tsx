1e57606216b4f1e3f25899d024cda3e346873a34
stevelabs <stevelabs@stevelabsui-MacBookPro-2.local>
2022-10-07 14:18:32 +0900

    [view]가이드 페이지 버튼 스타일 수정

--- components/Main/MyEstimateProject.tsx
+++ components/Main/MyEstimateProject.tsx
@@ -142,14 +142,14 @@ const CountBox = styled(Box)`
 `;
 
 const Count = styled(Typography)`
-  color: #caccd1;
   font-family: 'Spoqa Han Sans Neo';
   font-size: 27pt;
   font-weight: 700;
   line-height: 25.5pt;
   letter-spacing: -0.02em;
   margin-right: 3pt;
-  text-align: right;
+  text-align: right;    
+  color: #5a2dc9;
 
   @media (max-width: 899pt) {
     font-size: 15pt;
