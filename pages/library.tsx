d929b553a561812bdccf140c3265328c76402ab8
whljm1003 <whljm1003@stevelabs.co>
2022-10-11 14:46:52 +0900

    [view]엔티즌 도서관, LNB 수정

--- pages/library.tsx
+++ pages/library.tsx
@@ -3,8 +3,7 @@ import MypageHeader from 'components/mypage/request/header';
 import Image from 'next/image';
 import React from 'react';
 import colors from 'styles/colors';
-import arrowImg from 'public/images/blueArrow16.png';
-import newImg from 'public/images/new-label.png';
+import arrowImg from 'public/images/blueArrow.png';
 import { useRouter } from 'next/router';
 import WebHeader from 'web-components/WebHeader';
 import WebFooter from 'web-components/WebFooter';
@@ -17,33 +16,35 @@ const Library = () => {
 
   return (
     <WebBody>
-    <WebHeader />
-        <Inner>
-      <MypageHeader back={true} title={'엔티즌 도서관'} />
-      <Body>
-        {[1, 1, 1, 1, 1, 1, 1].map((_, index) => (
-          <List key={index} onClick={HandleOnClick}>
-            <div className="badge-img">
-              {/* <Image src={} alt="badge" /> */}
-            </div>
-            <div className="text-box">
-              <div>추후 문구 추가</div>
-              <div className="color-text">
-                자세히 보기
-                <div className="arrow-img">
-                  <Image src={arrowImg} alt="arrow" layout="fill" />
+      <WebHeader />
+      <Inner>
+        <MypageHeader back={true} title={'엔티즌 도서관'} />
+        <WebInnerHeader>엔티즌 도서관</WebInnerHeader>
+        <Body>
+          {[1, 1, 1, 1, 1, 1, 1].map((_, index) => (
+            <List key={index} onClick={HandleOnClick}>
+              <div className="badge-img">
+                {/* <Image src={} alt="badge" /> */}
+              </div>
+              <div className="text-box">
+                <div>엔티즌을 더 잘 쓰는 꿀팁 확인하기</div>
+                <div className="color-box">
+                  <div className="color-text">자세히 보기</div>
+
+                  <span className="arrow-img">
+                    <Image src={arrowImg} alt="arrow" layout="fill" />
+                  </span>
                 </div>
               </div>
-            </div>
-            {/* 뉴 아이콘 */}
-            {/* <NewIcon>
+              {/* 뉴 아이콘 */}
+              {/* <NewIcon>
               <Image src={newImg} alt="new-icon" />
             </NewIcon> */}
-          </List>
-        ))}
-      </Body>
+            </List>
+          ))}
+        </Body>
       </Inner>
-        <WebFooter />
+      <WebFooter />
     </WebBody>
   );
 };
@@ -56,11 +57,23 @@ const WebBody = styled.div`
   width: 100%;
   height: 100vh;
   margin: 0 auto;
-  //height: 810pt;
   background: #fcfcfc;
   @media (max-height: 809pt) {
     display: block;
     height: 100%;
+  }
+`;
+const WebInnerHeader = styled.h1`
+  font-weight: 700;
+  font-size: 25.5pt;
+  line-height: 37.5pt;
+  letter-spacing: -0.02em;
+  color: ${colors.main2};
+  padding-top: 55.5pt;
+  padding-bottom: 45pt;
+  text-align: center;
+  @media (max-width: 899pt) {
+    display: none;
   }
 `;
 const Body = styled.ul`
@@ -71,25 +84,21 @@ const Body = styled.ul`
   width: 100%;
   justify-content: space-between;
   @media (max-width: 899pt) {
-  //padding-top: 12pt;
-  position: relative;
-  //padding-left: 9.75pt;
-  padding-right: 0pt;
-  box-shadow: 0px 0px 10px 0px #89a3c933;
-  border-radius: 6pt;
-  display: block;
+    position: relative;
+    padding-right: 0pt;
+    box-shadow: 0px 0px 10px 0px #89a3c933;
+    border-radius: 6pt;
+    display: block;
   }
 `;
-
 const Inner = styled.div`
   display: block;
   position: relative;
   width: 900pt;
   height: 100%;
-  //width: 281.25pt;
   border-radius: 12pt;
-  margin: 45.75pt auto ;
-
+  margin-top: 0;
+  margin: 0 auto 45.75pt auto;
   @media (max-width: 899pt) {
     width: 100%;
     position: relative;
@@ -98,19 +107,15 @@ const Inner = styled.div`
     background: none;
   }
 `;
-
-
 const List = styled.li`
   display: flex;
-  gap: 7.5pt;
+  gap: 15pt;
   width: 436.5pt;
   height: 120pt;
   box-shadow: 0px 0px 10px rgba(137, 163, 201, 0.2);
   border-radius: 16px;
-  padding: 13.5pt 0 12pt;
   align-items: center;
   margin-bottom: 22.5pt;
-
   .badge-img {
     width: 75pt;
     height: 75pt;
@@ -122,47 +127,62 @@ const List = styled.li`
   .text-box {
     display: flex;
     flex-direction: column;
-    gap: 3pt;
+    gap: 9.75pt;
     font-weight: 400;
-    font-size: 12pt;
+    font-size: 19.5pt;
     line-height: 18pt;
     letter-spacing: -0.02em;
     color: ${colors.main2};
   }
-  .color-text {
+  .color-box {
     display: flex;
-    font-weight: 500;
-    font-size: 9pt;
-    line-height: 15pt;
+    align-items: center;
     letter-spacing: -0.02em;
     color: ${colors.main};
   }
+  .color-text {
+    position: relative;
+    top: 1.2pt; //  가운데 정렬 디테일 수정
+    font-weight: 500;
+    font-size: 13.5pt;
+    line-height: 15pt;
+  }
   .arrow-img {
     position: relative;
-    top: 1.87pt;
-    width: 9pt;
-    height: 9pt;
+    width: 15pt;
+    height: 15pt;
   }
-
   @media (max-width: 899pt) {
-  width:100% ;
-  margin-bottom: 0;
-  height: auto;
-  border-bottom: 0.75pt solid ${colors.lightGray};
-  box-shadow: none;
-  border-radius: 0;
-  .badge-img {
-    width: 44px;
-    height: 44px;
-    margin-left: 15pt;
+    width: 100%;
+    margin-bottom: 0;
+    border-radius: 0;
+    height: auto;
+    gap: 7.5pt;
+    border-bottom: 0.75pt solid ${colors.lightGray};
+    box-shadow: none;
+    padding: 13.5pt 0 12pt;
+    .text-box {
+      font-size: 12pt;
+      gap: 3pt;
+    }
+    .badge-img {
+      width: 44px;
+      height: 44px;
+      margin-left: 15pt;
+    }
+    .color-text {
+      top: 0.7pt; //  가운데 정렬 디테일 수정
+      font-size: 9pt;
+    }
+    .arrow-img {
+      width: 9pt;
+      height: 9pt;
+    }
   }
-  }
-  
 `;
 const NewIcon = styled.div`
   position: absolute;
 `;
-
 
 /*
  */
