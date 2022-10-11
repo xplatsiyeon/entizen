e04166af4dd053cba55a38a52d03acb300755e1b
whljm1003 <whljm1003@stevelabs.co>
2022-10-11 15:59:07 +0900

    [view]LNB 수정

--- web-components/WebHeader.tsx
+++ web-components/WebHeader.tsx
@@ -9,8 +9,10 @@ import Bell from 'public/images/bell.png';
 import BellOutline from 'public/images/Bell_outline.png';
 import Frame from 'public/images/Frame.png';
 import GuideLink from 'components/GuideLink';
+import ProfileUp from 'public/images/profile-up.png';
+import ProfileDown from 'public/images/profile-down.png';
 import { Router, useRouter } from 'next/router';
-
+import { handleLogoutOnClickModalClick } from 'api/auth/logout';
 
 type Props = {
   num?: number;
@@ -20,18 +22,26 @@ type Props = {
 const WebHeader = ({ num, now }: Props) => {
   const [linklist, setLinklist] = useState<boolean>(false);
   const [type, setType] = useState<string>('');
+  const [isHovering, setIsHovered] = useState(false);
+  const onMouseEnter = () => setIsHovered(true);
+  const onMouseLeave = () => setIsHovered(false);
 
   const router = useRouter();
 
-   const isUser= localStorage.getItem('USER_ID');
-   //if(!isUser)localStorage.setItem('USER_ID','user'). 유저 테스트용 코드
+  const isUser = localStorage.getItem('USER_ID');
+  //if(!isUser)localStorage.setItem('USER_ID','user'). 유저 테스트용 코드
 
-   const handleLink =()=>{
-    if(isUser){router.push('/quotation/request')}
-    else{router.push('/signin')}
-   }
+  const handleLink = () => {
+    if (isUser) {
+      router.push('/quotation/request');
+    } else {
+      router.push('/signin');
+    }
+  };
 
-
+  useEffect(() => {
+    console.log(`isUser-> ${isUser}`);
+  }, [isUser]);
   return (
     <>
       <Wrapper>
@@ -45,9 +55,7 @@ const WebHeader = ({ num, now }: Props) => {
                   </a>
                 </Link>
               </LogoBox>
-              <DivBox onClick={handleLink}>
-                간편견적
-              </DivBox>
+              <DivBox onClick={handleLink}>간편견적</DivBox>
               <DivBox
                 onClick={() => {
                   setLinklist(!linklist);
@@ -56,8 +64,8 @@ const WebHeader = ({ num, now }: Props) => {
               >
                 가이드
               </DivBox>
-              <DivBox onClick={()=>alert('2차 작업 범위입니다')}>
-                  소통하기
+              <DivBox onClick={() => alert('2차 작업 범위입니다')}>
+                소통하기
               </DivBox>
               <DivBox
                 onClick={() => {
@@ -71,42 +79,85 @@ const WebHeader = ({ num, now }: Props) => {
             <Box2>
               {/* <DivBox2><input type="text" placeholder="서비스를 검색해보세요" /> </DivBox2> */}
 
-              {isUser?
-              <>
-              <DivBox2>
-              <IconBox>
-                <Image src={Chat} alt="question" onClick={()=>router.push('/faq')} />
-              </IconBox>
-              <IconBox>
-                <Image src={BellOutline} alt="bell on" onClick={()=>router.push('/alarm')}/>
-              </IconBox>
-              <IconBox>
-                <Image src={Frame} alt="frame" />
-              </IconBox>
-              </DivBox2>
-              </>
-              :<>
-              <DivBox2>
-              <IconBox>
-                <Image src={Chat} alt="question" onClick={()=>router.push('/faq')}/>
-              </IconBox>
-              <IconBox>
-                <Image src={Bell} alt="alram" onClick={()=>router.push('/alarm')}/>
-              </IconBox>
-              </DivBox2>
-              <DivBox2>
-                <Link href="/signin">
-                  <a>로그인</a>
-                </Link>
-              </DivBox2>
-              <DivBox2>
-                {/*
+              {!isUser ? (
+                <>
+                  <DivBox2>
+                    <IconBox>
+                      <Image
+                        src={Chat}
+                        alt="question"
+                        onClick={() => router.push('/faq')}
+                      />
+                    </IconBox>
+                    <IconBox>
+                      <Image
+                        src={BellOutline}
+                        alt="bell on"
+                        onClick={() => router.push('/alarm')}
+                      />
+                    </IconBox>
+                    <ProfileBox
+                      onMouseEnter={onMouseEnter}
+                      onMouseLeave={onMouseLeave}
+                    >
+                      <IconBox>
+                        <Image src={Frame} alt="frame" />
+                      </IconBox>
+                      {isHovering ? (
+                        <UpDown>
+                          <ProfileMenu>
+                            <li onClick={() => router.push('/profile/editing')}>
+                              프로필 변경
+                            </li>
+                            <li onClick={() => router.push('/setting')}>
+                              설정
+                            </li>
+                            <li onClick={handleLogoutOnClickModalClick}>
+                              로그아웃
+                            </li>
+                          </ProfileMenu>
+                          <Image src={ProfileUp} alt="up" layout="fill" />
+                        </UpDown>
+                      ) : (
+                        <UpDown>
+                          <Image src={ProfileDown} alt="down" layout="fill" />
+                        </UpDown>
+                      )}
+                    </ProfileBox>
+                  </DivBox2>
+                </>
+              ) : (
+                <>
+                  <DivBox2>
+                    <IconBox>
+                      <Image
+                        src={Chat}
+                        alt="question"
+                        onClick={() => router.push('/faq')}
+                      />
+                    </IconBox>
+                    <IconBox>
+                      <Image
+                        src={Bell}
+                        alt="alram"
+                        onClick={() => router.push('/alarm')}
+                      />
+                    </IconBox>
+                  </DivBox2>
+                  <DivBox2>
+                    <Link href="/signin">
+                      <a>로그인</a>
+                    </Link>
+                  </DivBox2>
+                  <DivBox2>
+                    {/*
                 <Link href="/signUp/Terms"> */}
-                <Link href="/testTest"> 
-                  <a>회원가입</a>
-                </Link>
-              </DivBox2>
-              </>}
+                    <Link href="/testTest">
+                      <a>회원가입</a>
+                    </Link>
+                  </DivBox2>
+                </>
+              )}
             </Box2>
           </Inner>
         </MainLink>
@@ -158,13 +209,48 @@ const LogoBox = styled.div`
 
 const IconBox = styled.div`
   display: flex;
+  position: relative;
   flex: 1;
-  margin-right: 15pt;
+  margin-left: 15pt;
   width: 21pt;
-  height:21pt;
-  cursor: pointer; 
-  &:nth-last-of-type(1){
+  height: 21pt;
+  cursor: pointer;
+  &:nth-last-of-type(1) {
     margin-right: 0;
+  }
+`;
+const ProfileBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  height: 100%;
+`;
+const UpDown = styled.div`
+  position: relative;
+  width: 9pt;
+  height: 9pt;
+`;
+const ProfileMenu = styled.ul`
+  position: absolute;
+  top: 20pt;
+  right: -30pt;
+  z-index: 10;
+  width: 86.25pt;
+  background: ${colors.lightWhite};
+  box-shadow: 0px 0px 7.5pt rgba(137, 163, 201, 0.2);
+  border-radius: 6pt;
+  padding: 15pt 0;
+  text-align: center;
+  & > li {
+    :not(:nth-of-type(1)) {
+      margin-top: 18pt;
+    }
+    font-weight: 500;
+    font-size: 10.5pt;
+    line-height: 12pt;
+    letter-spacing: -0.02em;
+    color: ${colors.main2};
   }
 `;
 
