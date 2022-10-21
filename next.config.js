1a1b9abe72c211591459a7d9269bbbb2b41e7a2a
whljm1003 <whljm1003@stevelabs.co>
2022-10-21 17:31:25 +0900

    [fix] next.config error 수정

--- next.config.js
+++ next.config.js
@@ -5,14 +5,15 @@ const nextConfig = {
   env: {
     BASE_URL: process.env.BASE_URL,
   },
+  images: {
+    domains: ['https://test-entizen.s3.ap-northeast-2.amazonaws.com'],
+  },
+
   async rewrites() {
     return [
       {
-        source: ['/oauth2.0/:path*', '/:path*'], // url이 source에 해당될 경우
-        destination: [
-          'https://nid.naver.com/oauth2.0/:path*',
-          'https://example/:path*',
-        ], // destination으로 redirect
+        source: '/oauth2.0/:path*', // url이 source에 해당될 경우
+        destination: 'https://nid.naver.com/oauth2.0/:path*', // destination으로 redirect
       },
     ];
   },
