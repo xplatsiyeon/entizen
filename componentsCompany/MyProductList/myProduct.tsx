f661a124b742703c005c915470726c740c7ebdee
whljm1003 <whljm1003@stevelabs.co>
2022-10-26 12:46:31 +0900

    [view] 판매자 - 내 제품 리스트 상세페이지 CSS 수정

--- componentsCompany/MyProductList/myProduct.tsx
+++ componentsCompany/MyProductList/myProduct.tsx
@@ -48,8 +48,7 @@ const MyProduct = (props: Props) => {
     {
       staleTime: 5000,
       cacheTime: Infinity,
-      // enabled: router.isReady,
-      enabled: false,
+      enabled: router.isReady,
     },
   );
 
