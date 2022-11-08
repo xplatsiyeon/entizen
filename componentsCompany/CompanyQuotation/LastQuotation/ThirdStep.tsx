eb07b28788917c7a8e7ed7faf9a515978010b72f
whljm1003 <whljm1003@stevelabs.co>
2022-11-08 10:52:16 +0900

    [feat] 최종견적 POST

--- componentsCompany/CompanyQuotation/LastQuotation/ThirdStep.tsx
+++ componentsCompany/CompanyQuotation/LastQuotation/ThirdStep.tsx
@@ -176,7 +176,7 @@ const ThirdStep = ({
             subscribeTypeEn,
             subscribeProduct,
           ),
-          subscribePeriod: Number(subscribePeriod.slice(0, 2)),
+          subscribePeriod: subscribePeriod.slice(0, 2),
           userInvestRate: Number(userInvestRate) / 100 + '',
           chargingPointRate: Number(chargingPointRate) / 100 + '',
           subscribePricePerMonth: Number(
