a04faba3225483bcf7e60ae9d1c3b2d409d7dfcf
whljm1003 <whljm1003@stevelabs.co>
2022-11-11 10:43:26 +0900

    [feat] 그래프ql 데이터 테스트

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,13 +12,31 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
+  createHttpLink,
   gql,
 } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [queryClient] = useState(() => new QueryClient());
-  const client = new ApolloClient({
+
+  const httpLink = createHttpLink({
     uri: 'https://test-api.entizen.kr/api/graphql',
+  });
+  const authLink = setContext((_, { headers }) => {
+    // get the authentication token from local storage if it exists
+    const token = localStorage.getItem('token');
+    // return the headers to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : '',
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
   });
 
