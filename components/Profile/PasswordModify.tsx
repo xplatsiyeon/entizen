0bea9ba264de3e85d02c78aa21391ff5cf6b9e41
whljm1003 <whljm1003@stevelabs.co>
2022-12-01 23:17:07 +0900

    API 변경

--- components/Profile/PasswordModify.tsx
+++ components/Profile/PasswordModify.tsx
@@ -86,7 +86,7 @@ const PasswordModify = ({ setTabNumber }: Props) => {
   const handleClick = () => {
     const accessToken = JSON.parse(localStorage.getItem('ACCESS_TOKEN')!);
     // const memberIdx = JSON.parse(localStorage.getItem('MEMBER_IDX')!);
-    const PASSWORD_CHANGE = `https://api.entizen.kr/api/members/password/${key.memberIdx}`;
+    const PASSWORD_CHANGE = `https://test-api.entizen.kr/api/members/password/${key.memberIdx}`;
     try {
       axios({
         method: 'patch',
