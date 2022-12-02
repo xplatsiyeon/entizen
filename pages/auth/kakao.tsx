3d6bf402285256d85b5fd22b8297e74ea64115e8
whljm1003 <whljm1003@stevelabs.co>
2022-12-02 10:33:23 +0900

    API 주소 변경

--- pages/auth/kakao.tsx
+++ pages/auth/kakao.tsx
@@ -6,9 +6,12 @@ import { kakaoInit } from 'utils/kakao';
 
 const Auth = () => {
   const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
+  // 테스트 리다이렉트
   // const REDIRECT_URI = 'http://localhost:3000/kakaoAuth';
-  // const REDIRECT_URI = 'https://test-api.entizen.kr/auth/kakao';
-  const REDIRECT_URI = 'https://api.entizen.kr/auth/kakao';
+  const REDIRECT_URI = 'https://test-api.entizen.kr/auth/kakao';
+  // 라이브 리다이렉트 주소
+  // const REDIRECT_URI = 'https://api.entizen.kr/auth/kakao';
+
   const CLIENT_SECRET = 'asdasdakhj1243789123798';
   // calllback으로 받은 인가코드
   const code = new URL(window.location.href).searchParams.get('code');
