cabd2de106672e6bdeba25a8de0b30c342115611
whljm1003 <whljm1003@stevelabs.co>
2022-12-05 17:23:18 +0900

    가견적 API 변경

--- componentsCompany/Profile/profileEditing.tsx
+++ componentsCompany/Profile/profileEditing.tsx
@@ -122,7 +122,7 @@ const ProfileEditing = ({ setComponent }: Props) => {
   // useEffect(() => {
   //   axios({
   //     method: 'post',
-  //     url: 'https://test-api.entizen.kr/api/auth/nice',
+  //     url: 'https://api.entizen.kr/api/auth/nice',
   //     data: { memberType: token.memberType },
   //   })
   //     .then((res) => {
