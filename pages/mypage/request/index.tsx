df25f381b35935624b45c2e4756dd3f9302eef1a
whljm1003 <whljm1003@stevelabs.co>
2022-12-08 16:19:28 +0900

    최종견적 다른 파트너 리스트 목록 수정

--- pages/mypage/request/index.tsx
+++ pages/mypage/request/index.tsx
@@ -95,7 +95,7 @@ const Mypage1_3 = ({}: any) => {
   const [partnerModal, setPartnerModal] = useState(false);
   const [modalNumber, setModalNumber] = useState(-1);
   const [modalMessage, setModalMessage] = useState('');
-  const [isFinalItmeIndex, setIsFinalItmeIndex] = useState<number>(-2);
+  const [isFinalItmeIndex, setIsFinalItmeIndex] = useState<number>(-1);
   //----------- 구매자 내견적 상세 조회 API ------------
   const { data, isError, isLoading, refetch } =
     useQuery<QuotationRequestsResponse>(
@@ -272,8 +272,6 @@ const Mypage1_3 = ({}: any) => {
           currentInProgressPreQuotationIdx!
         ) {
           setIsFinalItmeIndex(index);
-        } else {
-          setIsFinalItmeIndex(-1);
         }
       });
     }
