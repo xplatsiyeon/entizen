d8475564dd5bd8db6d94f086c88933c51f9c0019
jsmsue010 <jsmsue010@stevelabs.co>
2022-12-26 12:09:36 +0900

    관리자 파트너 제품 등록 헤더1

--- componentsAdmin/PartnerProducts/PartnerProductsList.tsx
+++ componentsAdmin/PartnerProducts/PartnerProductsList.tsx
@@ -1,10 +1,87 @@
+import styled from "@emotion/styled";
+import colors from "styles/colors";
 
 const PartnerProductsList =()=>{
 
     return(
         <>
+           <Search>
+        <li className="search">
+          <label>검색</label>
+          <SearchBox
+            type="text"
+            placeholder="검색"
+          />
+          <Btn>
+            <Text
+            >
+              조회
+            </Text>
+          </Btn>
+        </li>
+      </Search>
         </>
     )
 }
 
 export default PartnerProductsList;
+
+
+const Search = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  label {
+    width: 50px;
+    margin-right: 39.75pt;
+  }
+  li {
+    gap: 7.5pt;
+    display: flex;
+    align-items: center;
+    background: #ffffff;
+    border: 1px solid ${colors.lightWhite3};
+    height: 30pt;
+    padding: 4pt 0 4pt 12pt;
+    width: 100%;
+  }
+
+  .search {
+    width: 946px;
+  }
+`;
+
+const Btn = styled.button`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  border-radius: 3pt;
+  padding: 5px 17px;
+  height: 19.5pt;
+  background: #e2e5ed;
+  border: 1px solid #747780;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 150%;
+  outline: none;
+  text-align: center;
+`;
+
+const Text = styled.span`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 150%;
+  color: #747780;
+  text-align: center;
+`;
+
+const SearchBox = styled.input`
+  border: 1px solid ${colors.lightWhite3};
+  height: 100%;
+  width: 274.5pt;
+  border-radius: 4px;
+  ::placeholder {
+    padding-left: 10px;
+  }
+`;
