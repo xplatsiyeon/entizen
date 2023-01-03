cb24907550fb829adbcfccd95428286d51e0c1e3
whljm1003 <whljm1003@stevelabs.co>
2023-01-03 17:54:36 +0900

    bridge test

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,11 +60,11 @@ const Home: NextPage<Props> = ({}: Props) => {
       // 아이폰 호출 테스트
     } else if (userAgent === 'iOS_App') {
       window.returnUserInfo = (userInfo) => {
-        alert('iOS 테스트 중..');
+        // alert('iOS 테스트 중..');
         if (typeof userInfo === 'object') {
-          alert('userInfo 호출');
+          // alert('userInfo 호출');
           // const jsonGetUserInfo = JSON.parse(userInfo);
-          alert(userInfo.USER_ID);
+          // alert(userInfo.USER_ID);
           sessionStorage.setItem(
             'SNS_MEMBER',
             JSON.stringify(userInfo.SNS_MEMBER),
