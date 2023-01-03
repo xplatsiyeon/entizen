b67b7802414e2b87943e86488a4c4c7099eecf4c
whljm1003 <whljm1003@stevelabs.co>
2023-01-03 12:22:34 +0900

    bridge test

--- bridge/appToWeb.ts
+++ bridge/appToWeb.ts
@@ -3,9 +3,11 @@ export const requestPermissionCheck = (
   userAgent: string,
   type: 'photo' | 'file',
 ) => {
-  if (userAgent === 'Android_App') {
-    window.entizen!.requestPermissionCheck(type);
-  } else if (userAgent === 'iOS_App') {
-    window.webkit.messageHandlers.requestPermissionCheck.postMessage(type);
+  if (userAgent.length > 1) {
+    if (userAgent === 'Android_App') {
+      window.entizen!.requestPermissionCheck(type);
+    } else if (userAgent === 'iOS_App') {
+      window.webkit.messageHandlers.requestPermissionCheck.postMessage(type);
+    }
   }
 };
