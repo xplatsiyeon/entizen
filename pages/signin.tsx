1e351587673a2c4c7ea85383a59dafdf4d034ec6
jsmsue010 <jsmsue010@stevelabs.co>
2023-01-16 12:08:00 +0900

    test server

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -58,9 +58,9 @@ export interface FindKey {
 
 const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
 // 테스트 리다이렉트 주소
-const REDIRECT_URI = 'https://api.entizen.kr/auth/kakao';
+const REDIRECT_URI = 'https://test-api.entizen.kr/auth/kakao';
 // 라이브 리다이렉트 주소
-// const REDIRECT_URI = 'https://api.entizen.kr/auth/kakao';
+// const REDIRECT_URI = 'https://test-api.entizen.kr/auth/kakao';
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 const Signin = () => {
@@ -194,7 +194,7 @@ const Signin = () => {
   };
   // 네이버 로그인
   const NaverApi = async (data: any) => {
-    const NAVER_POST = `https://api.entizen.kr/api/members/login/sns`;
+    const NAVER_POST = `https://test-api.entizen.kr/api/members/login/sns`;
     try {
       await axios({
         method: 'post',
@@ -332,7 +332,7 @@ const Signin = () => {
     const memberType = loginTypeEnList[selectedLoginType];
     axios({
       method: 'post',
-      url: 'https://api.entizen.kr/api/auth/nice',
+      url: 'https://test-api.entizen.kr/api/auth/nice',
       data: { memberType },
     })
       .then((res) => {
@@ -418,7 +418,7 @@ const Signin = () => {
           src="https://appleid.cdn-apple.com/appleauth/static/jsapi/appleid/1/en_US/appleid.auth.js"
         ></script>
         <meta name="appleid-signin-client-id" content="entizenapplekey" />
-        <meta name="appleid-signin-redirect-uri" content="https://api.entizen.kr/api/auth/apple" />
+        <meta name="appleid-signin-redirect-uri" content="https://test-api.entizen.kr/api/auth/apple" />
         <meta name="appleid-signin-scope" content="name email" />
         <meta name="appleid-signin-state" content="" />
         <meta name="appleid-signin-use-popup" content="true" />
