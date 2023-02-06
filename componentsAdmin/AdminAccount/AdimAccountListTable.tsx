ec84a9a1df4e8f48f438fa7bf474d1f52b24aa52
whljm1003 <whljm1003@stevelabs.co>
2023-02-06 11:16:17 +0900

    잰킨스 에러 핸들링

--- componentsAdmin/AdminAccount/AdimAccountListTable.tsx
+++ componentsAdmin/AdminAccount/AdimAccountListTable.tsx
@@ -39,7 +39,7 @@ const AdimAccountListTable = ({
 
   // 오늘 날짜.
   const today = new Date();
-  console.log(adminDateFomat(String(today)));
+  // console.log(adminDateFomat(String(today)));
 
   // 역경매 견적서 보기에 넘겨줄 아이디값
   const dispatch = useDispatch();
