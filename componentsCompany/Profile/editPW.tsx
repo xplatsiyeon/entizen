4cdef87eedae434365cefc5b7be1973429f2a293
KyeongyeonYun <cadyky95@stevelabs.co>
2023-02-09 15:51:53 +0900

    프로필 변경 경로 수정

--- componentsCompany/Profile/editPW.tsx
+++ componentsCompany/Profile/editPW.tsx
@@ -241,8 +241,10 @@ const EditPW = ({ setComponent }: Props) => {
           handle={setComponent ? true : undefined}
           back={true}
           title={'비밀번호 변경'}
-          handleOnClick={setComponent ? () => setComponent(1) : undefined}
-          handleBackClick={setComponent ? () => setComponent(1) : undefined}
+          // handleOnClick={setComponent ? () => setComponent(1) : undefined}
+          // handleBackClick={setComponent ? () => setComponent(1) : undefined}
+          handleOnClick={setComponent ? () => setComponent(0) : undefined}
+          handleBackClick={setComponent ? () => setComponent(0) : undefined}
         />
       </Wrap>
 
