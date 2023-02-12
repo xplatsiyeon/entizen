5e016273ac225792fddc9a0cab12eeb0e25eceea
whljm1003 <whljm1003@stevelabs.co>
2023-02-12 18:32:01 +0900

    이미지 다운로드 테스트

---
+++ pages/imgtest.tsx
@@ -0,0 +1,96 @@
+import { Button, Stack } from '@mui/material';
+import axios from 'axios';
+import JSZip from 'jszip';
+
+interface IDownloadProps {
+  fileName?: string;
+  url?: string;
+  data?: string | Blob;
+}
+
+const download = ({ fileName, data, url }: IDownloadProps) => {
+  const aTag = document.createElement('a');
+  let url_internal = '';
+  if (url) {
+    url_internal = url;
+  } else if (data) {
+    if (!fileName) return;
+    if (typeof data === 'string') {
+      url_internal = URL.createObjectURL(new Blob([data]));
+    } else {
+      url_internal = URL.createObjectURL(data);
+    }
+  } else return;
+  aTag.setAttribute('href', url_internal);
+  aTag.setAttribute('download', fileName || '');
+  aTag.click();
+  URL.revokeObjectURL(url_internal);
+};
+
+const downloadPng = async ({ fileName, url }: IDownloadProps) => {
+  await axios({
+    url: `${url}`,
+    method: 'GET',
+    responseType: 'blob',
+    withCredentials: false,
+  })
+    .then(({ data }) => {
+      download({ fileName: fileName, data });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+function App() {
+  let zip = new JSZip();
+  const data: string[] = [
+    'https://test-entizen.s3.ap-northeast-2.amazonaws.c…75925576_15ecadb5-9d33-4c13-965b-af5d398c306b.png',
+
+    'https://test-entizen.s3.ap-northeast-2.amazonaws.com/chatting/1675925576_66785699-e5dc-4207-a577-0e5daac98b66.png',
+
+    'https://test-entizen.s3.ap-northeast-2.amazonaws.c…75925576_e5019113-d37f-48c9-bf57-02f55192e12f.png',
+  ];
+  return (
+    <Stack direction="row" spacing={2}>
+      <Button
+        variant="contained"
+        onClick={async () => {
+          for (const i in data) {
+            await downloadPng({ fileName: `test_${i}.jpg`, url: data[i] });
+          }
+        }}
+      >
+        Download Image files
+      </Button>
+      <Button
+        variant="contained"
+        onClick={async () => {
+          for (const i in data) {
+            await axios({
+              url: `${data[i]}`,
+              method: 'GET',
+              responseType: 'arraybuffer',
+              withCredentials: false,
+            })
+              .then(async ({ data }) => {
+                const filename = `image_${i}.jpg`;
+                zip.file(filename, data, { binary: true });
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          }
+          zip.generateAsync({ type: 'blob' }).then((content) => {
+            console.log(`${content}`);
+            download({ fileName: `test_download.zip`, data: content });
+          });
+        }}
+      >
+        Download Zip file
+      </Button>
+    </Stack>
+  );
+}
+
+export default App;
