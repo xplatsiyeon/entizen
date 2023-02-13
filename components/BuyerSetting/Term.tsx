1e22d8a42b1592502011f8dfa68e7c67b902430a
KyeongyeonYun <cadyky95@stevelabs.co>
2023-02-13 10:27:20 +0900

    develop 최신화

--- components/BuyerSetting/Term.tsx
+++ components/BuyerSetting/Term.tsx
@@ -44,11 +44,32 @@ const Term = ({ tabNumber, setTabNumber, nowWidth }: Props) => {
 
       <Wrapper>
         <Title>
-          <span>엔티즌 이용약관</span>
+          <span>엔티즌 플랫폼 서비스 이용약관</span>
         </Title>
         <Subtitle>
-          <span>엔티즌 플랫폼 개인정보처리방침</span>
+          <span>제 1장 환영합니다!</span>
         </Subtitle>
+        <SubSubtitle>
+          <span>제 1조 (목적)</span>
+        </SubSubtitle>
+        <Content>
+          저희 엔티즌 서비스 및 제품(이하 통칭하여 ‘서비스’ 또는 ‘엔티즌
+          서비스’)을 이용해 주셔서 감사 합니다. 본 약관은 서비스의 이용과
+          관련하여 서비스를 제공하는
+          <span className="boldText">엘에스일렉트릭(주)</span>(이하 ‘회사’)과
+          이를 이용하는 회원 또는 비회원과의 관계를 설명합니다. 뿐만 아니라,
+          여러분이 엔티즌을 이용하 는 데 도움이 될 만한 유익한 정보를 포함하고
+          있습니다.
+          <br />
+          <br />
+          엔티즌 서비스를 이용하시거나 회원으로 가입하시는 경우 여러분은 본
+          약관에 동의하게 되므로, 잠시 시간을 내시어 본 약관의 내용을 살펴봐
+          주시기 바랍니다.
+        </Content>
+        <Title style={{ marginTop: '100pt' }}>
+          <span>엔티즌 플랫폼 개인정보처리방침</span>
+        </Title>
+
         <Content>
           <Line />
           <ul>
@@ -381,6 +402,9 @@ const Content = styled.div`
       padding-top: 10pt;
     }
     padding-bottom: 5pt;
+  }
+  .boldText {
+    font-weight: 600;
   }
 `;
 
