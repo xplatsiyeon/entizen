4fad003d0c6bdde22e71ab070fd0e57165d8f6aa
whljm1003 <whljm1003@stevelabs.co>
2023-02-16 11:57:42 +0900

    [feat] 애플 로그인 회원가입 연동

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -61,7 +61,7 @@ export interface FindKey {
 }
 
 const REST_API_KEY = process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY;
-const REDIRECT_URI = 'https://api.entizen.kr/auth/kakao';
+const REDIRECT_URI = 'https://test-api.entizen.kr/auth/kakao';
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 const Signin = () => {
@@ -229,76 +229,71 @@ const Signin = () => {
   };
   // 네이버 로그인
   const NaverApi = async (data: any) => {
-    const NAVER_POST = `https://api.entizen.kr/api/members/login/sns`;
-    try {
-      await axios({
-        method: 'post',
-        url: NAVER_POST,
-        data: {
-          uuid: '' + data.user.id,
-          snsType: 'NAVER',
-          snsResponse: JSON.stringify(data),
-          email: data.user.email,
-        },
+    const NAVER_POST = `https://test-api.entizen.kr/api/members/login/sns`;
+    await axios({
+      method: 'post',
+      url: NAVER_POST,
+      data: {
+        uuid: '' + data.user.id,
+        snsType: 'NAVER',
+        snsResponse: JSON.stringify(data),
+        email: data.user.email,
+      },
 
-        headers: {
-          ContentType: 'application/json',
-        },
-        withCredentials: true,
-      }).then((res) => {
-        console.log('[axios] 리스폰스 => ');
-        console.log(res);
-        console.log(res.data);
-        // const match = res.config.data.match(/\((.*)\)/);
-        let c = res.data;
-        let d = JSON.parse(res.config.data);
-        console.log('signin.tsx 65번째줄 axios 부분입니다 ! ======');
-        console.log(c);
-        dispatch(
-          userAction.add({
-            ...user,
-            uuid: d.uuid,
-            email: d.email,
-            snsType: d.snsType,
-            snsLoginIdx: c.snsLoginIdx,
-            isMember: c.isMember,
-          }),
-        );
-        if (c.isMember === true) {
-          const token: JwtTokenType = jwt_decode(res.data.accessToken);
-          localStorage.setItem('SNS_MEMBER', JSON.stringify(token.isSnsMember));
-          localStorage.setItem('USER_ID', JSON.stringify(data.user.email));
-          console.log(user.email);
-          localStorage.setItem('ACCESS_TOKEN', JSON.stringify(c.accessToken));
-          localStorage.setItem('REFRESH_TOKEN', JSON.stringify(c.refreshToken));
-          dispatch(originUserAction.set(data.user.email));
+      headers: {
+        ContentType: 'application/json',
+      },
+      withCredentials: true,
+    }).then((res) => {
+      console.log('[axios] 리스폰스 => ');
+      console.log(res);
+      console.log(res.data);
+      // const match = res.config.data.match(/\((.*)\)/);
+      let c = res.data;
+      let d = JSON.parse(res.config.data);
+      console.log('signin.tsx 65번째줄 axios 부분입니다 ! ======');
+      console.log(c);
+      dispatch(
+        userAction.add({
+          ...user,
+          uuid: d.uuid,
+          email: d.email,
+          snsType: d.snsType,
+          snsLoginIdx: c.snsLoginIdx,
+          isMember: c.isMember,
+        }),
+      );
+      if (c.isMember === true) {
+        const token: JwtTokenType = jwt_decode(res.data.accessToken);
+        localStorage.setItem('SNS_MEMBER', JSON.stringify(token.isSnsMember));
+        localStorage.setItem('USER_ID', JSON.stringify(data.user.email));
+        console.log(user.email);
+        localStorage.setItem('ACCESS_TOKEN', JSON.stringify(c.accessToken));
+        localStorage.setItem('REFRESH_TOKEN', JSON.stringify(c.refreshToken));
+        dispatch(originUserAction.set(data.user.email));
 
-          // ================브릿지 연결=====================
-          const userInfo = {
-            SNS_MEMBER: token.isSnsMember,
-            MEMBER_TYPE: token.memberType,
-            ACCESS_TOKEN: res.data.accessToken,
-            REFRESH_TOKEN: res.data.refreshToken,
-            USER_ID: data.user.email,
-          };
-          console.log('==========userInfo==========');
-          console.log(userInfo);
-          if (userAgent === 'Android_App') {
-            window.entizen!.setUserInfo(JSON.stringify(userInfo));
-          } else if (userAgent === 'iOS_App') {
-            window.webkit.messageHandlers.setUserInfo.postMessage(
-              JSON.stringify(userInfo),
-            );
-          }
-          router.push('/');
-        } else {
-          router.push('/signUp/SnsTerms');
+        // ================브릿지 연결=====================
+        const userInfo = {
+          SNS_MEMBER: token.isSnsMember,
+          MEMBER_TYPE: token.memberType,
+          ACCESS_TOKEN: res.data.accessToken,
+          REFRESH_TOKEN: res.data.refreshToken,
+          USER_ID: data.user.email,
+        };
+        console.log('==========userInfo==========');
+        console.log(userInfo);
+        if (userAgent === 'Android_App') {
+          window.entizen!.setUserInfo(JSON.stringify(userInfo));
+        } else if (userAgent === 'iOS_App') {
+          window.webkit.messageHandlers.setUserInfo.postMessage(
+            JSON.stringify(userInfo),
+          );
         }
-      });
-    } catch (error) {
-      console.log('post 요청 실패');
-      console.log(error);
-    }
+        router.push('/');
+      } else {
+        router.push('/signUp/SnsTerms');
+      }
+    });
   };
   // 네이버 온클릭
   const handleNaver = async () => {
@@ -397,7 +392,7 @@ const Signin = () => {
     const memberType = loginTypeEnList[selectedLoginType];
     axios({
       method: 'post',
-      url: 'https://api.entizen.kr/api/auth/nice',
+      url: 'https://test-api.entizen.kr/api/auth/nice',
       data: { memberType },
     })
       .then((res) => {
@@ -496,14 +491,8 @@ const Signin = () => {
       console.log(token);
       const base64Payload = token.split('.')[1]; //value 0 -> header, 1 -> payload, 2 -> VERIFY SIGNATURE
       const payload = Buffer.from(base64Payload, 'base64');
-      const result = JSON.parse(payload.toString());
-      console.log('애플로그인 user 유니크값 : ', result);
-      if (data.detail.authorization.user.email) {
-        console.log(
-          '애플로그인 user 유니크값 : ',
-          data.detail.authorization.user.email,
-        );
-      }
+      const result: any = JSON.parse(payload.toString());
+      handleAppleLogin(result);
     });
     //애플로 로그인 실패 시.
     document.addEventListener('AppleIDSignInOnFailure', (error) => {
@@ -512,6 +501,81 @@ const Signin = () => {
       //todo fail logic
     });
   }, []);
+
+  // 애플로그인 핸들러
+  const handleAppleLogin = async (result: any) => {
+    console.log('애플로그인 user 유니크값 : ', result);
+    const email = data.detail.authorization.user.email
+      ? data.detail.authorization.user.email
+      : '';
+
+    const APPLE_POST = `https://test-api.entizen.kr/api/members/login/sns`;
+    await axios({
+      method: 'post',
+      url: APPLE_POST,
+      data: {
+        uuid: '' + data.detail.authorization.user.sub,
+        snsType: 'NAVER',
+        snsResponse: JSON.stringify(result),
+        email: email,
+      },
+
+      headers: {
+        ContentType: 'application/json',
+      },
+      withCredentials: true,
+    }).then((res) => {
+      console.log('[axios] 리스폰스 => ');
+      console.log(res);
+      console.log(res.data);
+      // const match = res.config.data.match(/\((.*)\)/);
+      let c = res.data;
+      let d = JSON.parse(res.config.data);
+      console.log('signin.tsx 65번째줄 axios 부분입니다 ! ======');
+      console.log(c);
+      dispatch(
+        userAction.add({
+          ...user,
+          uuid: d.uuid,
+          email: d.email,
+          snsType: d.snsType,
+          snsLoginIdx: c.snsLoginIdx,
+          isMember: c.isMember,
+        }),
+      );
+      if (c.isMember === true) {
+        const token: JwtTokenType = jwt_decode(res.data.accessToken);
+        localStorage.setItem('SNS_MEMBER', JSON.stringify(token.isSnsMember));
+        localStorage.setItem('USER_ID', JSON.stringify(data.user.email));
+        console.log(user.email);
+        localStorage.setItem('ACCESS_TOKEN', JSON.stringify(c.accessToken));
+        localStorage.setItem('REFRESH_TOKEN', JSON.stringify(c.refreshToken));
+        dispatch(originUserAction.set(data.user.email));
+
+        // ================브릿지 연결=====================
+        const userInfo = {
+          SNS_MEMBER: token.isSnsMember,
+          MEMBER_TYPE: token.memberType,
+          ACCESS_TOKEN: res.data.accessToken,
+          REFRESH_TOKEN: res.data.refreshToken,
+          USER_ID: data.user.email,
+        };
+        console.log('==========userInfo==========');
+        console.log(userInfo);
+        if (userAgent === 'Android_App') {
+          window.entizen!.setUserInfo(JSON.stringify(userInfo));
+        } else if (userAgent === 'iOS_App') {
+          window.webkit.messageHandlers.setUserInfo.postMessage(
+            JSON.stringify(userInfo),
+          );
+        }
+        router.push('/');
+      } else {
+        router.push('/signUp/SnsTerms');
+      }
+    });
+  };
+
   // 유저타입 확인하는 useEffect
   useEffect(() => {
     if (selectedLoginType === 0) {
@@ -541,7 +605,7 @@ const Signin = () => {
         <meta name="appleid-signin-client-id" content="entizenapplekey" />
         <meta
           name="appleid-signin-redirect-uri"
-          content="https://api.entizen.kr/api/auth/apple"
+          content="https://test-api.entizen.kr/api/auth/apple"
         />
         <meta name="appleid-signin-scope" content="name email" />
         <meta name="appleid-signin-state" content="" />
