9fddad52ba6824a1d373cb92be380f4c19725173
KyeongyeonYun <cadyky95@stevelabs.co>
2023-02-20 18:43:59 +0900

    2차 디자인 수정 53번

--- components/Modal/RequestModal.tsx
+++ components/Modal/RequestModal.tsx
@@ -36,10 +36,10 @@ const RequestModal = ({
           <H1>{title}</H1>
           <Text>{subtitle}</Text>
           <BtnBox>
-            <LeftBtn onClick={leftControl}>탈퇴</LeftBtn>
             <RightBtn border={border} onClick={rightControl}>
               다시 생각해 볼게요
             </RightBtn>
+            <LeftBtn onClick={leftControl}>계정 탈퇴</LeftBtn>
           </BtnBox>
         </Modal>
       ) : (
@@ -74,20 +74,25 @@ const Modal = styled.div<{ border?: boolean }>`
   background: ${colors.lightWhite};
   box-shadow: 3pt 0 7.5pt rgba(137, 163, 201, 0.2);
   border-radius: ${({ border }) => (border ? '' : '22.5pt 22.5pt 0 0')};
-  padding: 30pt 15pt;
+  padding: 30pt 15pt 10pt;
 
   @media (max-width: 899.25pt) {
     position: absolute;
     bottom: 0;
+    width: 220.5pt;
+    border-radius: 6pt;
+    bottom: 150pt;
   }
 
   @media (min-width: 900pt) {
-    border-radius: 16px;
-    width: 560px;
+    border-radius: 12pt;
+    width: 354pt;
+    padding: 42pt 28.5pt 10pt;
   }
 `;
 const H1 = styled.h1`
   white-space: pre-wrap;
+  font-family: ' Spoqa Han Sans Neo';
   font-weight: 700;
   font-size: 15pt;
   line-height: 21pt;
@@ -96,27 +101,42 @@ const H1 = styled.h1`
   color: ${colors.main2};
   @media (min-width: 900pt) {
     white-space: normal;
+    font-size: 21pt;
+    font-weight: 700;
+    line-height: 33pt;
+    letter-spacing: -0.02em;
+    text-align: center;
   }
 `;
 const Text = styled.p`
   white-space: pre-wrap;
+  font-family: 'Spoqa Han Sans Neo';
   font-weight: 400;
-  font-size: 12pt;
-  line-height: 18pt;
+  font-size: 10.5pt;
+  line-height: 15pt;
   text-align: center;
   letter-spacing: -0.02em;
   color: ${colors.main2};
   padding-top: 12pt;
+
+  @media (min-width: 900pt) {
+    font-size: 12pt;
+    font-weight: 400;
+    line-height: 18pt;
+    letter-spacing: -0.02em;
+    text-align: center;
+  }
 `;
 const BtnBox = styled.div`
   text-align: center;
   padding-top: 24pt;
   display: flex;
   justify-content: center;
+  flex-direction: column;
   gap: 9pt;
 `;
 const LeftBtn = styled(Button)`
-  background: ${colors.gray};
+  /* background: ${colors.gray}; */
   border-radius: 6pt;
   font-weight: 700;
   font-size: 12pt;
@@ -126,10 +146,11 @@ const LeftBtn = styled(Button)`
   color: ${colors.darkGray};
   padding: 15pt 26.25pt;
   @media (min-width: 900pt) {
-    width: 102pt;
+    width: 100%;
   }
 `;
 const RightBtn = styled(Button)<{ border?: boolean }>`
+  font-family: 'Spoqa Han Sans Neo';
   background: ${colors.main};
   border-radius: 6pt;
   font-weight: 700;
@@ -140,6 +161,11 @@ const RightBtn = styled(Button)<{ border?: boolean }>`
   color: ${colors.lightWhite};
   padding: ${({ border }) => (border ? '15pt 37.5pt' : '15pt 72.75pt')};
   @media (min-width: 900pt) {
-    width: 230pt;
+    width: 100%;
+    font-size: 12pt;
+    font-weight: 700;
+    line-height: 12pt;
+    letter-spacing: -0.02em;
+    text-align: center;
   }
 `;
--- componentsCompany/MyProductList/ProductList.tsx
+++ componentsCompany/MyProductList/ProductList.tsx
@@ -6,9 +6,10 @@ import Image from 'next/image';
 import { useRouter } from 'next/router';
 import carImg from 'public/images/carImage.png';
 import plusBtn from 'public/images/productPlus.png';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 import colors from 'styles/colors';
+import NoProductList from 'public/images/NothingProductListSvg.svg';
 
 type Props = {};
 export interface File {
@@ -50,6 +51,8 @@ const ProductList = (props: Props) => {
   );
   const router = useRouter();
 
+  const [isData, setIsData] = useState(false);
+
   // 숫자만 추출해주는 함수
   const onlyNumber = (target: string) => {
     const testString = target;
@@ -77,52 +80,80 @@ const ProductList = (props: Props) => {
   console.log(data);
   console.log('⭐️ 내 제품리스트 데이터 ~line 65 -> ' + TAG);
 
+  useEffect(() => {
+    if (data?.chargerProduct?.length === 0) {
+      setIsData(false);
+    } else if (data?.chargerProduct?.length !== undefined) {
+      setIsData(true);
+    }
+  }, [data]);
+
   return (
     <>
       <Wrapper>
-        {data?.chargerProduct?.map((item, index) => (
-          <ListBox
-            key={index}
-            onClick={() =>
-              router.push({
-                pathname: '/company/myProductList/detail',
-                query: {
-                  chargerProductIdx: item?.chargerProductIdx,
-                },
-              })
-            }
-          >
-            {item?.chargerImageFiles !== undefined ? (
-              <ImageBox>
-                <Image
-                  src={item?.chargerImageFiles[0]?.url}
-                  alt="carImage"
-                  layout="fill"
-                  priority={true}
-                  unoptimized={true}
-                  objectFit="cover"
-                />
-              </ImageBox>
-            ) : (
-              <NoImageBox />
-            )}
+        {data?.chargerProduct?.length === 0 ? (
+          <>
+            <ImgBox>
+              <ImgTag
+                src="/images/NothingProductListSvg.svg"
+                alt="NoProductLIst"
+              />
+              <NoTitle>등록된 제품이 없습니다.</NoTitle>
 
-            <DisplayBox>
-              <TextBox>
-                <Title>{item.modelName}</Title>
-                <From>{`제조사: ${item.manufacturer}`}</From>
-              </TextBox>
-              <KwBox>{`${onlyNumber(item.kind)} kW`}</KwBox>
-            </DisplayBox>
-          </ListBox>
-        ))}
+              <NoContents>
+                내 제품을 등록하고,
+                <br /> 견적 작성 시간을 줄여보세요!
+              </NoContents>
+            </ImgBox>
+          </>
+        ) : (
+          <>
+            {data?.chargerProduct?.map((item, index) => (
+              <ListBox
+                key={index}
+                onClick={() =>
+                  router.push({
+                    pathname: '/company/myProductList/detail',
+                    query: {
+                      chargerProductIdx: item?.chargerProductIdx,
+                    },
+                  })
+                }
+              >
+                {item?.chargerImageFiles !== undefined ? (
+                  <ImageBox>
+                    <Image
+                      src={item?.chargerImageFiles[0]?.url}
+                      alt="carImage"
+                      layout="fill"
+                      priority={true}
+                      unoptimized={true}
+                      objectFit="cover"
+                    />
+                  </ImageBox>
+                ) : (
+                  <NoImageBox />
+                )}
+
+                <DisplayBox>
+                  <TextBox>
+                    <Title>{item.modelName}</Title>
+                    <From>{`제조사: ${item.manufacturer}`}</From>
+                  </TextBox>
+                  <KwBox>{`${onlyNumber(item.kind)} kW`}</KwBox>
+                </DisplayBox>
+              </ListBox>
+            ))}
+          </>
+        )}
       </Wrapper>
-      <BtnBox>
+      <BtnBox isData={isData}>
         <ProductAddBtn onClick={() => router.push('/company/addProduct')}>
-          <div>
+          {/* <div>
             <Image src={plusBtn} alt="plus" />
           </div>
-          <div>제품 추가하기</div>
+          <div>제품 추가하기</div> */}
+          <span className="text">내 제품 등록하기</span>
         </ProductAddBtn>
       </BtnBox>
     </>
@@ -137,7 +168,6 @@ const Wrapper = styled.div`
   flex-direction: column;
   gap: 9pt;
   box-sizing: border-box;
-
   /* height: 100vh; */
 `;
 
@@ -214,19 +244,20 @@ const KwBox = styled.div`
   border-radius: 12pt;
 `;
 
-const BtnBox = styled.div`
+const BtnBox = styled.div<{ isData: boolean }>`
   padding-left: 15pt;
   padding-right: 15pt;
   box-sizing: border-box;
   position: fixed;
-  bottom: 30pt;
+  /* bottom: 30pt; */
+  bottom: 34.5pt;
   width: 100%;
   cursor: pointer;
   @media (min-width: 900pt) {
-    position: relative;
+    position: static;
     margin: 0 auto;
     width: 254.7pt;
-    padding-top: 150pt;
+    padding-top: ${({ isData }) => (isData === true ? '150pt' : '42pt')};
   }
 `;
 
@@ -239,6 +270,11 @@ const ProductAddBtn = styled.div`
   justify-content: center;
   border-radius: 6pt;
   background-color: ${colors.main};
+  width: 120.75pt;
+  margin: 0 auto;
+  @media (min-width: 900pt) {
+    width: 100%;
+  }
   & div:first-of-type {
     width: 15pt;
     height: 15pt;
@@ -252,10 +288,84 @@ const ProductAddBtn = styled.div`
     text-align: center;
     color: #ffffff;
   }
+  .text {
+    color: #ffffff;
+    font-family: 'Spoqa Han Sans Neo';
+    font-size: 12pt;
+    font-weight: 700;
+    line-height: 12pt;
+    letter-spacing: -0.02em;
+    text-align: center;
+
+    @media (max-width: 899.25pt) {
+      font-size: 9pt;
+      font-weight: 700;
+      line-height: 12pt;
+      letter-spacing: -0.02em;
+      text-align: left;
+    }
+  }
 `;
 
 const DisplayBox = styled.div`
   display: flex;
   align-items: center;
+`;
+
+const ImgBox = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 83.25pt;
+  @media (min-width: 900pt) {
+    padding-top: 0;
+  }
+`;
+
+const ImgTag = styled.img`
+  width: 38.25pt;
+  @media (min-width: 900pt) {
+    width: 60pt;
+  }
+`;
+
+const NoTitle = styled.span`
+  font-family: ' Spoqa Han Sans Neo';
+  color: #a6a9b0;
+  font-size: 12pt;
+  font-weight: 500;
+  line-height: 12pt;
+  letter-spacing: -0.02em;
+  text-align: center;
+  padding-top: 16.5pt;
+  @media (min-width: 900pt) {
+    padding-top: 28pt;
+    font-size: 18pt;
+    font-weight: 500;
+    line-height: 15pt;
+    letter-spacing: -0.02em;
+    text-align: center;
+  }
+`;
+
+const NoContents = styled.span`
+  font-family: ' Spoqa Han Sans Neo';
+  color: #a6a9b0;
+  font-size: 9pt;
+  font-weight: 500;
+  line-height: 12pt;
+  letter-spacing: -0.02em;
+  text-align: center;
+  padding-top: 7.5pt;
+  @media (min-width: 900pt) {
+    padding-top: 15pt;
+    font-size: 12pt;
+    font-weight: 500;
+    line-height: 18pt;
+    letter-spacing: -0.02em;
+    text-align: center;
+    white-space: nowrap;
+  }
 `;
 export default ProductList;
