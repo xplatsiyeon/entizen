90dcf9417af0fb3ca1b7f9c23360df8c9ed223fa
KyeongyeonYun <cadyky95@stevelabs.co>
2023-02-22 19:15:54 +0900

    1차 디자인 반려 수정

--- componentsWeb/WebHeader.tsx
+++ componentsWeb/WebHeader.tsx
@@ -3,7 +3,8 @@ import styled from '@emotion/styled';
 import Image from 'next/image';
 import Link from 'next/link';
 import colors from 'styles/colors';
-import Logos from 'public/images/webLogo.png';
+// import Logos from 'public/images/webLogo.png';
+import Logos from 'public/images/EntizenHeaderLogoSvg.svg';
 import Chat from 'public/images/chat.png';
 //알람 꺼짐
 import Bell from 'public/images/bell.png';
