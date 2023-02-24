41d68fd9804e6865f973d5d054287b9e40470b79
whljm1003 <whljm1003@stevelabs.co>
2023-02-24 18:39:34 +0900

    모두싸인 API 변경

--- api/getDocument.ts
+++ api/getDocument.ts
@@ -5,14 +5,11 @@ export const getDocument = (documentId: string) => {
   const fetch = require('node-fetch');
 
   const url = `https://api.modusign.co.kr/documents/${documentId}/embedded-view`;
-  // const url = `https://api.modusign.co.kr/documents/${documentId}`;
   const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      authorization:
-        'Basic ZW50aXplbkBlbnRpemVuLmtyOk5XWXpPRGc0WldNdE1Ua3haQzAwWkRnMkxUaGpPR010T1dOaVpEWTROR0l6TlRZMA==',
-      // `Basic ${process.env.NEXT_PUBLIC_MODUSIGN_KEY}==`,
+      authorization: `Basic ${process.env.NEXT_PUBLIC_MODUSIGN_KEY}==`,
     },
   };
 
@@ -40,9 +37,7 @@ export const modusignPdfDown = (documentId: string) => {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      authorization:
-        // 'Basic ZW50aXplbkBlbnRpemVuLmtyOk5XWXpPRGc0WldNdE1Ua3haQzAwWkRnMkxUaGpPR010T1dOaVpEWTROR0l6TlRZMA==',
-        `Basic ${process.env.NEXT_PUBLIC_MODUSIGN_KEY}==`,
+      authorization: `Basic ${process.env.NEXT_PUBLIC_MODUSIGN_KEY}==`,
     },
   };
 
