8b1e1a4837cb7e62dc42164b94b548fb9fd908ce
KyeongyeonYun <cadyky95@stevelabs.co>
2023-03-03 15:38:15 +0900

    폰트 3번째 페이지가지 수정

--- components/Landing/LandingThird.tsx
+++ components/Landing/LandingThird.tsx
@@ -120,7 +120,8 @@ const MobileTextWrapper = styled.div`
 `;
 
 const MainText = styled.span`
-  font-family: 'AppleSDGothicNeo', 'Noto Sans KR', sans-serif;
+  /* font-family: 'AppleSDGothicNeo', 'Noto Sans KR', sans-serif; */
+  font-family: 'AppleGothicNeo';
   font-size: 37.5pt;
   font-weight: 700;
   line-height: 52.5pt;
@@ -139,7 +140,8 @@ const MainText = styled.span`
 
 const SubText = styled.span`
   padding-top: 30pt;
-  font-family: 'AppleSDGothicNeo', 'Noto Sans KR', sans-serif;
+  /* font-family: 'AppleSDGothicNeo', 'Noto Sans KR', sans-serif; */
+  font-family: 'AppleGothicNeo';
   font-size: 15pt;
   font-weight: 500;
   line-height: 25.5pt;
