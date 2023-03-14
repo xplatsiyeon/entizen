f8a1c44a3e3f5a06837d9cae3a5fd254ee52517d
KyeongyeonYun <cadyky95@stevelabs.co>
2023-03-14 10:53:53 +0900

    editor 수정 중

--- componentsAdmin/AdminInformationNotify/Adminterms/AdminTibtapEditor.tsx
+++ componentsAdmin/AdminInformationNotify/Adminterms/AdminTibtapEditor.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import { useEditor, EditorContent } from '@tiptap/react';
+import Image from '@tiptap/extension-image';
+import StarterKit from '@tiptap/starter-kit';
+import TipTapMenu from './TipTapMenu';
+import styled from '@emotion/styled';
 
-const AdminTibtapEditor = () => {
-  return <div>AdminTibtapEditor</div>;
+type Props = {
+  setBodyText: React.Dispatch<React.SetStateAction<string>>;
+  bodyText: string;
+  firstContent?: string;
+};
+
+const AdminTibtapEditor = ({ setBodyText, bodyText, firstContent }: Props) => {
+  const editor = useEditor({
+    extensions: [StarterKit, Image],
+  });
+
+  return (
+    <Wrapper>
+      <TipTapMenu editor={editor} />
+      <EditorContent
+        editor={editor}
+        onChange={() => {
+          setBodyText(editor?.getHTML()!);
+        }}
+      />
+    </Wrapper>
+  );
 };
 
 export default AdminTibtapEditor;
+
+const Wrapper = styled.div`
+  .ProseMirror {
+    width: 100%;
+    height: 500px;
+    border: 1px solid red;
+    padding: 10px;
+  }
+`;
