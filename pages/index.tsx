4eba85ffc936c64eaeadf8f72ead607fd07b46ba
whljm1003 <whljm1003@stevelabs.co>
2023-04-04 13:19:42 +0900

    [fix] 브릿지 라우팅 시 로그인 유지 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -39,29 +39,31 @@ const Home: NextPage<Props> = ({}: Props) => {
     // 안드로이드 호출
     if (userAgent === 'Android_App') {
       window.returnUserInfo = (userInfo) => {
-        if (userInfo.length > 1) {
-          const jsonGetUserInfo = JSON.parse(userInfo);
-          sessionStorage.setItem(
-            'SNS_MEMBER',
-            JSON.stringify(jsonGetUserInfo.SNS_MEMBER),
-          );
-          sessionStorage.setItem(
-            'MEMBER_TYPE',
-            JSON.stringify(jsonGetUserInfo.MEMBER_TYPE),
-          );
-          sessionStorage.setItem(
-            'ACCESS_TOKEN',
-            JSON.stringify(jsonGetUserInfo.ACCESS_TOKEN),
-          );
-          sessionStorage.setItem(
-            'REFRESH_TOKEN',
-            JSON.stringify(jsonGetUserInfo.REFRESH_TOKEN),
-          );
-          sessionStorage.setItem(
-            'USER_ID',
-            JSON.stringify(jsonGetUserInfo.USER_ID),
-          );
-        }
+        setTimeout(() => {
+          if (userInfo.length > 1) {
+            const jsonGetUserInfo = JSON.parse(userInfo);
+            sessionStorage.setItem(
+              'SNS_MEMBER',
+              JSON.stringify(jsonGetUserInfo.SNS_MEMBER),
+            );
+            sessionStorage.setItem(
+              'MEMBER_TYPE',
+              JSON.stringify(jsonGetUserInfo.MEMBER_TYPE),
+            );
+            sessionStorage.setItem(
+              'ACCESS_TOKEN',
+              JSON.stringify(jsonGetUserInfo.ACCESS_TOKEN),
+            );
+            sessionStorage.setItem(
+              'REFRESH_TOKEN',
+              JSON.stringify(jsonGetUserInfo.REFRESH_TOKEN),
+            );
+            sessionStorage.setItem(
+              'USER_ID',
+              JSON.stringify(jsonGetUserInfo.USER_ID),
+            );
+          }
+        }, 2000);
 
         setLoginChecking(false);
       };
