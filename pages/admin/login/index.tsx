7446b2312c051d8026d0e0507a7095cdbeb56cc4
whljm0815 <whljm0815@stevelabs.co>
2023-07-13 16:23:59 +0900

    [fix] 로그인 자동완성 추가

--- pages/admin/login/index.tsx
+++ pages/admin/login/index.tsx
@@ -108,14 +108,14 @@ const AdLogin = () => {
           <p>로그인</p>
           <InputID
             type="text"
-            // autoComplete="new-password"
+            autoComplete="username"
             placeholder="아이디"
             onChange={(e) => changeValue(e)}
             ref={idRef}
           />
           <InputPW
             type="password"
-            // autoComplete="new-password"
+            autoComplete="current-password"
             placeholder="비밀번호"
             onChange={(e) => changeValue(e)}
             ref={pwRef}
