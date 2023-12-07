fb2fca457dfcbba77f0843f4c62035b220a2e19c
leewoo5629 <steve@x-plat.io>
2023-12-07 17:17:11 +0900

    zapier api working

--- next.config.js
+++ next.config.js
@@ -28,42 +28,20 @@ const nextConfig = {
         destination: 'https://nid.naver.com/oauth2.0/:path*', // destination으로 redirect
       },
       {
-        source: '/zapier/submit/:path*', // url이 source에 해당될 경우
+        source: '/zapier/submit/:path*', // 공용 폼 제출
         destination: 'https://hooks.zapier.com/hooks/catch/8791679/3f4shlg/:path*', // destination으로 redirect
       },
       {
-        source: '/zapier/submit-private/:path*', // url이 source에 해당될 경우
+        source: '/zapier/submit-private/:path*', // 비공용 폼 제출
         destination: 'https://hooks.zapier.com/hooks/catch/8791679/3f2b75d/:path*', // destination으로 redirect
       },
       {
-        source: '/zapier/company/:path*', // url이 source에 해당될 경우
+        source: '/zapier/company-request/:path*', // 요청업체 제출
         destination: 'https://hooks.zapier.com/hooks/catch/8791679/3f1qgqv/:path*', // destination으로 redirect
       },
       {
-        source: '/zapier/company-selection/:path*', // url이 source에 해당될 경우
+        source: '/zapier/company-selection/:path*', // 비공용:업체에 실사 신청하기
         destination: 'https://hooks.zapier.com/hooks/catch/8791679/3f40ngg/:path*', // destination으로 redirect
-      },
-    ];
-  },
-
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
-        ],
       },
     ];
   },
