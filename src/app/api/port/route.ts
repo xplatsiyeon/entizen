eb72216da0d2e2b8dcc5c17056e2e8eb9daf897f
hjkim8342 <hjkim8342@naver.com>
2024-04-26 17:17:21 +0900

    api 변경

--- src/app/api/port/route.ts
+++ src/app/api/port/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (req: NextRequest) => {
   try {
-    return NextResponse.json(API_CODE[200]);
+    return NextResponse.json(process.env.PORT);
   } catch (error) {
     return NextResponse.json(API_CODE[9000]);
   }
