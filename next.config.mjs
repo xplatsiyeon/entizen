d86fafe2e37a9d6abdfbd77bc857b2f7d822f2cd
hjkim8342 <hjkim8342@naver.com>
2024-05-03 12:13:02 +0900

    기존 소스 삭제 / 초기화

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "image.ohou.se",
-      },
-      {
-        protocol: "https",
-        hostname: "image.ohouse.com",
-      },
-    ],
-  },
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: "https",
+  //       hostname: "image.ohou.se",
+  //     },
+  //     {
+  //       protocol: "https",
+  //       hostname: "image.ohouse.com",
+  //     },
+  //   ],
+  // },
   reactStrictMode: false,
 
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/applyAd",
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/applyAd",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
--- src/bridge/webToApp.ts
+++
@@ -1,2 +0,0 @@
-// 앱 ---> 웹
-export const openGallery = () => {};
